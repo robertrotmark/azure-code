4314a093dc459dec36cc54b297734cd4ffa79122
Robert Rotmark <robert.rotmark@tele2.com>
2021-10-25 20:56:44 +0200

    Removed some unused code

--- Certificate/InstallCert.sh
+++ Certificate/InstallCert.sh
@@ -8,7 +8,5 @@ vname="<add keyvault name>"
 for file in *.crt; do # Whitespace-safe but not recursive.
     secretName=${file//./-}
     secretName=${secretName//-crt/}
-    #echo $secretName
-    #echo "Merge request \"$file\" with \"$secretName\""
     kubectl get secrets --all-namespaces |grep $secretName
 done
--- Certificate/MergeCert.sh
+++ Certificate/MergeCert.sh
@@ -7,8 +7,6 @@ vname="<add keyvault name>"
 for file in *.crt; do # Whitespace-safe but not recursive.
     secretName=${file//./-}
     secretName=${secretName//-crt/}
-    echo $secretName
-    echo "Merge request \"$file\" with \"$secretName\""
     az keyvault certificate pending merge --file $file --name $secretName --vault-name $vname
 done
 
